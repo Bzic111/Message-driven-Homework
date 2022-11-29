157f8d148165ddddbbe0d4c47ddc3da940f0f493
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2022-11-29 19:36:59 +0300

    Разбитие кода по файлам

---
+++ Message-driven Homework/Restaurant.cs
@@ -0,0 +1,48 @@
+﻿public class Restaurant
+{
+    private readonly List<Table> _tables = new List<Table>();
+    public Restaurant()
+    {
+        for (ushort i = 0; i < 10; i++)
+        {
+            _tables.Add(new Table(i));
+        }
+    }
+    public void BookFreeTable(int personsCount)
+    {
+        Console.WriteLine("Cheking tables");
+        var table = _tables.FirstOrDefault(t => t.SeatsCount >= personsCount && t.CurrentState is State.Free);
+        Thread.Sleep(1000 * 2);
+        string result;
+        if (table is null)
+        {
+            result = $"No tables";
+        }
+        else
+        {
+            table.SetState(State.Booked);
+            result = $"Yours table {table.Id}";
+        }
+        Console.WriteLine(result);
+    }
+    public void BookFreeTableAsync(int personsCount)
+    {
+        Console.WriteLine("Cheking tables");
+        Task.Run(async () =>
+        {
+            var table = _tables.FirstOrDefault(t => t.SeatsCount >= personsCount && t.CurrentState is State.Free);
+            await Task.Delay(1000 * 2);
+            string result;
+            if (table is null)
+            {
+                result = $"No tables";
+            }
+            else
+            {
+                table.SetState(State.Booked);
+                result = $"Yours table {table.Id}";
+            }
+            Console.WriteLine(result);
+        });
+    }
+}
---
+++ Message-driven Homework/Table.cs
@@ -0,0 +1,19 @@
+﻿public class Table
+{
+    public State CurrentState { get; private set; }
+    public int SeatsCount { get; }
+    public int Id { get; }
+    public Table(int id)
+    {
+        Id = id;
+        CurrentState = State.Free;
+        SeatsCount = new Random().Next(2, 5);
+    }
+    public bool SetState(State st)
+    {
+        if (st == CurrentState)
+            return false;
+        CurrentState = st;
+        return true;
+    }
+}
